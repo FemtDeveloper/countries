4e2b589d30172bea09f3a81a09540fabbc3c284b
Felix Miranda <feedmite@hotmail.com>
2022-02-12 15:58:41 -0500

    theme fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useSelector } from "react-redux";
 function App() {
   const isDarkMode = useSelector((state) => state.isDarkMode);
   return (
-    <Router>
+    <Router basename="/tothepoint_login">
       <div className={isDarkMode ? "dark-mode" : "App"}>
         <Navbar />
         <div className="countries">
